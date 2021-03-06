acd681c24dbd801edfa74045785a48009df94e0c
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-03-06 16:19:03 +0100

    burger

--- Maquette/burger.js
+++ Maquette/burger.js
@@ -4,12 +4,12 @@
     /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
     $('#burger_icon').click(function(e){
         e.preventDefault();
-        $('contenu').toggleClass('move_menu');
+        $('burger').toggleClass('move_menu');
     });
 
     /* Je veux pouvoir masquer le menu si on clique sur le cache */
     $('#site-cache').click(function(e){
-        $('contenu').removeClass('move_menu');
+        $('burger').removeClass('move_menu');
     })
 
 })(jQuery);
