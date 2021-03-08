c4805414133283d248ef3e46d8f9f34d69b2ec32
Enzo17101 <78074212+Enzo17101@users.noreply.github.com>
2021-03-08 12:51:10 +0100

    ICONE!

--- Maquette/annuaire/main.js
+++ Maquette/annuaire/main.js
@@ -5,10 +5,8 @@ function move_menu_burger()
   p.onclick = move_burger;
 };
 
-
 function close_menu_burger()
 {
   var p = document.getElementById("burger_cross");
   p.onclick = close_burger;
 };
-
